38fc0016707bbbe40211f1245794b24de2298d45
Sahaj Jain <82111591+jnsahaj@users.noreply.github.com>
2025-03-07 14:07:00 +0530

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -25,6 +25,7 @@ vim.opt.mouse = "a"
 vim.opt.showmode = false
 
 vim.keymap.set("n", "<leader>gg", "<cmd>:silent !tmux popup -B -w100\\% -h100\\% -E lazygit<CR>")
+vim.keymap.set("n", "<leader>cp", '<cmd>let @+ = expand("%")<CR>')
 
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
@@ -72,6 +73,8 @@ vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
+
+vim.opt.swapfile = false
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
@@ -992,6 +995,67 @@ require("lazy").setup({
 	},
 
 	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+
+	{
+		"yetone/avante.nvim",
+		event = "VeryLazy",
+		lazy = false,
+		version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+		opts = {
+			-- add any opts here
+			-- for example
+			provider = "openai",
+			openai = {
+				endpoint = "https://api.openai.com/v1",
+				model = "gpt-4o-mini", -- your desired model (or use gpt-4o, etc.)
+				timeout = 30000, -- timeout in milliseconds
+				temperature = 0, -- adjust if needed
+				max_tokens = 4096,
+				-- reasoning_effort = "high" -- only supported for reasoning models (o1, etc.)
+			},
+		},
+		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+		build = "make",
+		-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"stevearc/dressing.nvim",
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			--- The below dependencies are optional,
+			"echasnovski/mini.pick", -- for file_selector provider mini.pick
+			"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+			"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+			"ibhagwan/fzf-lua", -- for file_selector provider fzf
+			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+			"zbirenbaum/copilot.lua", -- for providers='copilot'
+			{
+				-- support for image pasting
+				"HakonHarnes/img-clip.nvim",
+				event = "VeryLazy",
+				opts = {
+					-- recommended settings
+					default = {
+						embed_image_as_base64 = false,
+						prompt_for_file_name = false,
+						drag_and_drop = {
+							insert_mode = true,
+						},
+						-- required for Windows users
+						use_absolute_path = true,
+					},
+				},
+			},
+			{
+				-- Make sure to set this up properly if you have lazy=true
+				"MeanderingProgrammer/render-markdown.nvim",
+				opts = {
+					file_types = { "markdown", "Avante" },
+				},
+				ft = { "markdown", "Avante" },
+			},
+		},
+	},
 
 	{ -- Collection of various small independent plugins/modules
 		"echasnovski/mini.nvim",
